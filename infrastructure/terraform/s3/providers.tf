e4d7a8d3755316ae9c902d762cb36caad6d8dc1b
Todd Punderson <github@sl.greyrock.io>
2023-01-13 17:55:01 -0500

    s3 terraform

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias          = "nas"
-  minio_server   = "s3.private.greyrock.io:9001"
-  minio_user     = module.onepassword_item_minio.fields.username
-  minio_password = module.onepassword_item_minio.fields.password
+  minio_server   = "s3.private.greyrock.io:9000"
+  minio_user     = module.onepassword_item_minio.fields.root_user_key
+  minio_password = module.onepassword_item_minio.fields.root_secret_key
 }
