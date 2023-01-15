6979fc0d40fee373d26db98274f2f4ce0a7cbfc5
Todd Punderson <github@sl.greyrock.io>
2023-01-15 10:42:25 -0500

    MORE NOPE

--- infrastructure/terraform/github/main.tf
+++ infrastructure/terraform/github/main.tf
@@ -31,9 +31,3 @@ module "onepassword_item_flux" {
   vault  = "Automation"
   item   = "flux"
 }
-
-module "onepassword_item_dockerhub" {
-  source = "github.com/Doonga/terraform-1password-item?ref=main"
-  vault  = "Automation"
-  item   = "docker-hub"
-}
