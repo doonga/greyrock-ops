2b7926e3505ce60253f4dfc0a8f17d6f60c162e4
Todd Punderson <github@sl.greyrock.io>
2023-01-25 11:46:02 -0500

    kubenav callback

--- infrastructure/terraform/auth0/modules/greyrock/client.tf
+++ infrastructure/terraform/auth0/modules/greyrock/client.tf
@@ -9,7 +9,7 @@ resource "auth0_client" "default" {
     "https://grafana.greyrock.tech/login/generic_oauth",        # Grafana
     "http://localhost:8000",                                # kubectl oidc-login
     "https://status.greyrock.io/authorization-code/callback", # Gatus
-    "https://kubenav.io/oidc.html",                         # Kubenav
+    "https://kubenav.io/auth/oidc.html",                         # Kubenav
     "https://gitops.greyrock.tech/oauth2/callback",             # Weave-Gitops
   ]
 
