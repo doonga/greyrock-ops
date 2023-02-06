9588eb7efdee4b04948d0bf201140be1a35d3f20
Todd Punderson <github@sl.greyrock.io>
2023-02-06 10:25:41 -0500

    use local ntp source in chrony

--- infrastructure/terraform/vyos/modules/config/containers-chrony.tf
+++ infrastructure/terraform/vyos/modules/config/containers-chrony.tf
@@ -7,5 +7,10 @@ resource "vyos_config" "container-chrony" {
         "address" = "${cidrhost(var.networks.services, 5)}"
       }
     }
+    "environment" = {
+      "NTP_SERVERS" = {
+        "value" = "${var.config.ntp_servers}"
+      }
+    }
   })
 }
