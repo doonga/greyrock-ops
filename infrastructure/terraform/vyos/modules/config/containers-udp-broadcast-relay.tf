790802f28f13a56b963d50cab93f879d42005374
Todd Punderson <sunny.song4198@nocanseemymail.com>
2023-02-06 13:33:24 -0500

    Get time machine working again

--- infrastructure/terraform/vyos/modules/config/containers-udp-broadcast-relay.tf
+++ infrastructure/terraform/vyos/modules/config/containers-udp-broadcast-relay.tf
@@ -7,7 +7,7 @@ resource "vyos_config" "container-udp-broadcast-relay-mdns" {
     "environment" = {
       "CFG_ID"        = { "value" = "2" }
       "CFG_PORT"      = { "value" = "5353" }
-      "CFG_DEV"       = { "value" = "${var.config.zones.trusted.interface};${var.config.zones.iot.interface}" }
+      "CFG_DEV"       = { "value" = "${var.config.zones.trusted.interface};${var.config.zones.iot.interface};${var.config.zones.servers.interface}" }
       "CFG_MULTICAST" = { "value" = "224.0.0.251" }
       "SEPARATOR"     = { "value" = ";" }
     }
