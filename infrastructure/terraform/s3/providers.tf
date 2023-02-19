3e7b1045b091ebf2419a6809f0757b2e61c2cfbe
Todd Punderson <sunny.song4198@nocanseemymail.com>
2023-02-19 15:04:05 -0500

    dump minio for garage

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,10 +1,3 @@
-provider "minio" {
-  alias          = "nas"
-  minio_server   = "s3.private.greyrock.io:9000"
-  minio_user     = module.onepassword_item_minio.fields.root_user_key
-  minio_password = module.onepassword_item_minio.fields.root_secret_key
-}
-
 provider "garage" {
   alias  = "nas"
   host   = "nas.private.greyrock.io:3911"
