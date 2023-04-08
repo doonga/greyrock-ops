15db1936cc1f8661aad9c6c2c671489a4be7508b
Todd Punderson <tpunderson@greyrock.io>
2023-04-08 18:36:40 -0400

    Add containers repo

--- infrastructure/terraform/github/repositories.tf
+++ infrastructure/terraform/github/repositories.tf
@@ -88,6 +88,38 @@ module "renovate-config" {
   )
 }
 
+module "container-images" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "container-images"
+  description = "Kubernetes tailored container images for various applications"
+  topics      = ["kubernetes", "docker", "containers"]
+  visibility  = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = merge(
+    {},
+    local.greyrock_bot_secrets
+  )
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [],
+    local.default_issue_labels
+  )
+}
+
 module "gh-workflows" {
   source = "mineiros-io/repository/github"
   version = "0.18.0"
