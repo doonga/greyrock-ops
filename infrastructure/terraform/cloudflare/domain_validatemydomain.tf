f7b65a326a8a98a31f473ed1218182e172c261f9
Todd Punderson <tpunderson@greyrock.io>
2023-05-05 13:37:36 -0400

    Add validatemydomain

---
+++ infrastructure/terraform/cloudflare/domain_validatemydomain.tf
@@ -0,0 +1,50 @@
+module "cf_domain_validatemydomain" {
+  source     = "./modules/cf_domain"
+  domain     = "validatemydomain.com"
+  account_id = cloudflare_account.greyrock.id
+  plan_type  = "free"
+  enable_email_routing = true
+  email_catch_all_address = "tpunderson+validatemydomain@greyrock.io"
+  dns_entries = [
+    # Cloudflare Email Routing
+    {
+      id       = "cloudflare_mx_1"
+      name     = "@"
+      priority = 81
+      value    = "route1.mx.cloudflare.net"
+      type     = "MX"
+    },
+    {
+      id       = "cloudflare_mx_2"
+      name     = "@"
+      priority = 63
+      value    = "route2.mx.cloudflare.net"
+      type     = "MX"
+    },
+    {
+      id       = "cloudflare_mx_3"
+      name     = "@"
+      priority = 87
+      value    = "route3.mx.cloudflare.net"
+      type     = "MX"
+    },
+    {
+      id    = "cloudflare_spf"
+      name  = "@"
+      value = "v=spf1 include:_spf.mx.cloudflare.net -all"
+      type  = "TXT"
+    },
+    {
+      id    = "cloudflare_dmarc"
+      name  = "_dmarc"
+      value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:f7c78cf7d42547b49cfdfcefc8d4cdb3@dmarc-reports.cloudflare.net;"
+      type  = "TXT"
+    },
+    {
+      id    = "cloudflare_dkim"
+      name  = "*._domainkey"
+      value = "v=DKIM1; p="
+      type  = "TXT"
+    }
+  ]
+}
