7ae686a01102fd325af15b48bae60e3b5badcbc1
Todd Punderson <tpunderson@greyrock.io>
2023-05-05 13:52:21 -0400

    Ingress email settings

--- infrastructure/terraform/cloudflare/domain_ingress.tf
+++ infrastructure/terraform/cloudflare/domain_ingress.tf
@@ -6,18 +6,46 @@ module "cf_domain_ingress" {
   enable_email_routing = true
   email_catch_all_address = "tpunderson+greyrock-tech@greyrock.io"
   dns_entries = [
-    # Generic settings
+  # Cloudflare Email Routing
     {
-      name  = "_dmarc"
-      value = "v=DMARC1; p=reject; rua=mailto:3iucjkde@ag.us.dmarcian.com;"
-      type  = "TXT"
+      id       = "cloudflare_mx_1"
+      name     = "@"
+      priority = 81
+      value    = "route1.mx.cloudflare.net"
+      type     = "MX"
     },
     {
-      id    = "ingress_spf"
+      id       = "cloudflare_mx_2"
+      name     = "@"
+      priority = 53
+      value    = "route2.mx.cloudflare.net"
+      type     = "MX"
+    },
+    {
+      id       = "cloudflare_mx_3"
+      name     = "@"
+      priority = 34
+      value    = "route3.mx.cloudflare.net"
+      type     = "MX"
+    },
+    {
+      id    = "cloudflare_spf"
       name  = "@"
-      value = "v=spf1 -all"
+      value = "v=spf1 include:_spf.mx.cloudflare.net -all"
       type  = "TXT"
     },
+    {
+      id    = "cloudflare_dmarc"
+      name  = "_dmarc"
+      value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:9ff38b3b2fea437ca0ec34207b72df2d@dmarc-reports.cloudflare.net;"
+      type  = "TXT"
+    },
+    {
+      id    = "cloudflare_dkim"
+      name  = "*._domainkey"
+      value = "v=DKIM1; p="
+      type  = "TXT"
+    }
     {
       id    = "azure_validator"
       name  = "@"
