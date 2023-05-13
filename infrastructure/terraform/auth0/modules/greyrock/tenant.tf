2d81602d88a1029369f522664cf736955a4f2afc
Todd Punderson <tpunderson@greyrock.io>
2023-05-13 17:52:24 -0400

    Auth0 update tenant settings

--- infrastructure/terraform/auth0/modules/greyrock/tenant.tf
+++ infrastructure/terraform/auth0/modules/greyrock/tenant.tf
@@ -1,3 +1,6 @@
 resource "auth0_tenant" "grey_rock" {
-  friendly_name   = "Grey Rock"
+  friendly_name       = "Grey Rock"
+  support_email       = "support@greyrock.io"
+  support_url         = "https://www.greyrock.io/support"
+  allowed_logout_urls = "https://auth.greyrock.tech/oauth2/sign_out"
 }
