7ef53799dceb28d01693b00554cf7a644d6eb6b5
Todd Punderson <tpunderson@greyrock.io>
2023-06-15 14:11:45 -0400

    Add vyos build repo

--- infrastructure/terraform/github/repositories.tf
+++ infrastructure/terraform/github/repositories.tf
@@ -247,3 +247,35 @@ module "vyos-config" {
     local.default_issue_labels
   )
 }
+
+module "vyos-build" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "vyos-build"
+  description = "Vyos rolling release builds"
+  topics      = ["vyos"]
+  visibility  = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+  is_template  = false
+
+  plaintext_secrets = merge(
+    {},
+    local.greyrock_bot_secrets
+  )
+
+  issue_labels_merge_with_github_labels = false
+  issue_labels = concat(
+    [],
+    local.default_issue_labels
+  )
+}
