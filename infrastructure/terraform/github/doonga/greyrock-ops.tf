9838ca361147012aa7d947cf2f2f4a44279efa40
Todd Punderson <tpunderson@greyrock.io>
2023-06-16 11:04:00 -0400

    Refactor

---
+++ infrastructure/terraform/github/doonga/greyrock-ops.tf
@@ -0,0 +1,53 @@
+module "greyrock_ops" {
+  source = "github.com/doonga/terraform-github-repository?ref=v1.1.0"
+
+  name         = "greyrock-ops"
+  description  = "My home or for-home infrastructure written as code, adhering to GitOps practices"
+  topics       = ["flux", "gitops", "iac", "k8s-at-home", "kubernetes", "renovate"]
+  homepage_url = "https://greyrock.github.io/greyrock-ops"
+  visibility   = "public"
+
+  auto_init              = true
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  has_issues   = true
+  has_wiki     = false
+  has_projects = false
+
+  plaintext_secrets = merge(
+    {},
+    local.greyrock_bot_secrets
+  )
+
+  issue_labels_manage_default_github_labels = false
+  issue_labels = concat(
+    [
+      { name = "area/ci", color = "72ccf3", description = "Issue relates to CI" },
+      { name = "area/kubernetes", color = "72ccf3", description = "Issue relates to Kubernetes" },
+
+      { name = "renovate/container", color = "ffc300", description = "Issue relates to a Renovate container update" },
+      { name = "renovate/helm", color = "ffc300", description = "Issue relates to a Renovate helm update" },
+    ],
+    local.issue_labels_semver,
+    local.issue_labels_size,
+    local.issue_labels_category
+  )
+
+  pages = {
+    custom_404 = true
+    build_type = "legacy"
+    branch     = "gh-pages"
+    path       = "/"
+  }
+
+  webhooks = [
+    {
+      events = ["push"]
+      url    = nonsensitive(var.secrets.flux_github_webhook_url)
+      secret = var.secrets.flux_github_webhook_secret
+    }
+  ]
+}
--- infrastructure/terraform/github/main.tf
+++ infrastructure/terraform/github/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "5.26.0"
+      version = "5.27.0"
     }
   }
 }
@@ -30,4 +30,55 @@ module "onepassword_item_flux" {
   source = "github.com/Doonga/terraform-1password-item?ref=main"
   vault  = "Automation"
   item   = "flux"
+}
+
+module "doonga" {
+  source = "./doonga"
+
+  secrets = {
+    greyrock_bot_app_id            = module.onepassword_item_github_greyrock_bot.fields.github_app_id
+    greyrock_bot_private_key       = module.onepassword_item_github_greyrock_bot.fields.github_app_private_key
+    flux_github_webhook_url    = module.onepassword_item_flux.fields.github_webhook_url
+    flux_github_webhook_secret = module.onepassword_item_flux.fields.github_webhook_token
+  }
+}
+
+moved {
+  from = module.vyos-config
+  to   = module.doonga.module.vyos_config
+}
+
+moved {
+  from = module.terraform-1password-item
+  to   = module.doonga.module.terraform_1password_item
+}
+
+moved {
+  from = module.gh-workflows
+  to   = module.doonga.module.gh_workflows
+}
+
+moved {
+  from = module.greyrock-ops
+  to   = module.doonga.module.home_ops
+}
+
+moved {
+  from = module.container-images
+  to   = module.doonga.module.container_images
+}
+
+moved {
+  from = module.esphome-config
+  to   = module.doonga.module.esphome_config
+}
+
+moved {
+  from = module.renovate-config
+  to   = module.doonga.module.renovate_config
+}
+
+moved {
+  from = module.terraform-github-repository
+  to   = module.doonga.module.terraform_github_repository
 }
