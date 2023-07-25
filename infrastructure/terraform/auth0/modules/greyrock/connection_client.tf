fa72889e3527812fcc456baa514d88ffd88b551c
Todd Punderson <tpunderson@greyrock.io>
2023-07-25 12:30:05 -0400

    Remove auth0

--- infrastructure/terraform/auth0/modules/greyrock/connection_client.tf
+++
@@ -1,9 +0,0 @@
-resource "auth0_connection_client" "passwordless_email_default" {
-  connection_id = auth0_connection.passwordless_email.id
-  client_id     = auth0_client.default.id
-}
-
-resource "auth0_connection_client" "passwordless_email_miniflux" {
-  connection_id = auth0_connection.passwordless_email.id
-  client_id     = auth0_client.miniflux.id
-}
