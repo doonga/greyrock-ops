b8f5b1f1c5c4b4371543e7f9ef0296b6766e7096
Todd Punderson <tpunderson@greyrock.io>
2023-09-14 10:18:46 -0400

    Massive catchup

--- infrastructure/talos/greyrock-ops/apply.sh
+++ infrastructure/talos/greyrock-ops/apply.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.4 -f ./clusterconfig/greyrock-ops-k8s1.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.5 -f ./clusterconfig/greyrock-ops-k8s2.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.6 -f ./clusterconfig/greyrock-ops-k8s3.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.7 -f ./clusterconfig/greyrock-ops-k8s4.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.8 -f ./clusterconfig/greyrock-ops-k8s5.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.9 -f ./clusterconfig/greyrock-ops-k8s6.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.71 -f ./clusterconfig/greyrock-ops-k8s1.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.72 -f ./clusterconfig/greyrock-ops-k8s2.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.73 -f ./clusterconfig/greyrock-ops-k8s3.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.74 -f ./clusterconfig/greyrock-ops-k8s4.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.75 -f ./clusterconfig/greyrock-ops-k8s5.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.76 -f ./clusterconfig/greyrock-ops-k8s6.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.77 -f ./clusterconfig/greyrock-ops-k8s7.greyrock.io.yaml
+talosctl apply-config -n 10.1.1.78 -f ./clusterconfig/greyrock-ops-k8s8.greyrock.io.yaml
