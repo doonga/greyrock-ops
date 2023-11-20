ae89a4342ed78d8ea64c869c4aadf1100b546393
Todd Punderson <fairs_rubs_0j@icloud.com>
2023-11-20 12:28:10 -0500

    Add some system settings

--- infrastructure/nix/modules/common/users/todd/darwin.nix
+++ infrastructure/nix/modules/common/users/todd/darwin.nix
@@ -1,4 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   users.users.todd.home = "/Users/todd";
+
+  fonts.fontDir.enable = true;
+  fonts.fonts = [(pkgs.nerdfonts.override {fonts = ["Meslo"];})];
+
+  system.defaults = {
+    finder.AppleShowAllExtensions = true;
+    finder._FXShowPosixPathInTitle = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.InitialKeyRepeat = 14;
+    NSGlobalDomain.KeyRepeat = 1;
+  };
 }
