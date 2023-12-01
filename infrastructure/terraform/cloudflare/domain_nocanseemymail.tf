3fafbf3c4b3ac6f333e62f4e67afb1ac0e033226
Todd Punderson <fairs_rubs_0j@icloud.com>
2023-12-01 16:37:26 -0500

    feat: Add masked email domain

---
+++ infrastructure/terraform/cloudflare/domain_nocanseemymail.tf
@@ -0,0 +1,73 @@
+module "cf_domain_main" {
+  source               = "./modules/cf_domain"
+  domain               = "nocanseemymail.com"
+  account_id           = cloudflare_account.greyrock.id
+  plan_type            = "free" # change to free when plan expires
+  enable_email_routing = false
+
+  dns_entries = [
+    # Generic settings
+    {
+      name  = "_dmarc"
+      value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:9fad2d90843e43ff94930d1289f5dead@dmarc-reports.cloudflare.net"
+      type  = "TXT"
+    },
+    # Fastmail settings
+    {
+      id       = "fastmail_mx_1"
+      name     = "@"
+      priority = 10
+      value    = "in1-smtp.messagingengine.com"
+      type     = "MX"
+    },
+    {
+      id       = "fastmail_mx_2"
+      name     = "@"
+      priority = 10
+      value    = "in2-smtp.messagingengine.com"
+      type     = "MX"
+    },
+    {
+      id      = "fastmail_dkim_1"
+      name    = "fm1._domainkey"
+      value   = "fm1.nocanseemymail.com.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id      = "fastmail_dkim_2"
+      name    = "fm2._domainkey"
+      value   = "fm2.nocanseemymail.com.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id      = "fastmail_dkim_3"
+      name    = "fm3._domainkey"
+      value   = "fm3.nocanseemymail.com.dkim.fmhosted.com"
+      type    = "CNAME"
+      proxied = false
+    },
+    {
+      id    = "fastmail_spf"
+      name  = "@"
+      value = "v=spf1 include:spf.messagingengine.com ?all"
+      type  = "TXT"
+    },
+  ]
+
+  waf_custom_rules = [
+    {
+      enabled     = true
+      description = "Firewall rule to block bots and threats determined by CF"
+      expression  = "(cf.client.bot) or (cf.threat_score gt 14)"
+      action      = "block"
+    },
+    {
+      enabled     = true
+      description = "Firewall rule to block certain countries"
+      expression  = "(ip.geoip.country in {\"CN\" \"IN\" \"KP\" \"RU\"})"
+      action      = "block"
+    },
+  ]
+}
