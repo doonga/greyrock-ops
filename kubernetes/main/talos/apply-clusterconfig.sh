70ee4bdb29e4b4505ddbd323cfbdda9469909d57
Todd Punderson <agile.car7544@undercovermail.org>
2024-02-15 09:32:02 -0500

    feat: Rework cluster / talos bootstrapping

--- kubernetes/main/talos/apply-clusterconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.4 -f ./clusterconfig/main-k8s1.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.5 -f ./clusterconfig/main-k8s2.greyrock.io.yaml
-talosctl apply-config -n 10.1.1.6 -f ./clusterconfig/main-k8s3.greyrock.io.yaml
